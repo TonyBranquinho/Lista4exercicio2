00fe99f74b4319434f914fcb68d14e65b2386bea
Tony <antony.branquinho@gmail.com>
2024-01-09 15:48:30 -0300

    legal

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program {
+        static void Main(string[] args) {
+
+            int n, dentro, fora, x;
+
+            Console.WriteLine("Digite um numero!");
+            n = int.Parse(Console.ReadLine());
+
+            dentro = 0;
+            fora = 0;
+            for (int i = 0; i < n; i++) {
+                x = int.Parse(Console.ReadLine());
+                if (x >= 10 && x <= 20) {
+                    dentro++;
+                }
+                else {
+                    fora++;
+                }
+                
+
+            }
+
+            Console.WriteLine(dentro + "Dentro");
+            Console.WriteLine(fora + "Fora");
+        }
+    }
+}
